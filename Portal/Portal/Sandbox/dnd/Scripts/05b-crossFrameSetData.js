7d4157db3b0a2066093dd20d6ecfcf1872524d83
wilk666 <andre@cartomatic.pl>
2014-07-17 16:57:46 +0200

    Sandboxed scripts
    but used in life app
    should be moved to proper scripts

---
+++ Portal/Portal/Sandbox/dnd/Scripts/05b-crossFrameSetData.js
@@ -0,0 +1,63 @@
+﻿
+var dragObject = new function () {
+    var me = this;
+
+    var targetNode;
+    var eventNoticeNode, dragEventNoticeNode;
+
+    var dataTransferCommentString;
+
+    me.init = function () {
+
+        if (EventHelpers.hasPageLoadHappened(arguments)) {
+            return;
+        }
+
+
+        targetNode = document.getElementById('dropTarget');
+        eventNoticeNode = document.getElementById('eventNotice');
+        dragEventNoticeNode = document.getElementById('dragEventNotice');
+
+        /* These are events for the draggable objects */
+        var dragNodes = cssQuery('[draggable=true]');
+        console.warn('ehhh', dragNodes);    
+        
+        for (var i = 0; i < dragNodes.length; i++) {
+            var dragNode = dragNodes[i]
+            EventHelpers.addEvent(dragNode, 'dragstart', dragStartEvent);
+        }
+
+        /* These are events for the object to be dropped */
+        if (targetNode) {
+            EventHelpers.addEvent(targetNode, 'dragover', dragOverEvent);
+            EventHelpers.addEvent(targetNode, 'drop', dropEvent);
+        }
+    }
+
+    function dragStartEvent(e) {
+        console.warn('dragStartEvent', this, e, sprintf('<img src="%s" alt="%s" /><br /><p class="caption">%s</p>',
+				this.src, this.alt, this.alt
+			));
+        e.dataTransfer.setData('Text',
+			sprintf('<img src="%s" alt="%s" /><br /><p class="caption">%s</p>',
+				e.explicitOriginalTarget.src, this.alt, this.alt
+			)
+		);
+
+    }
+
+
+    function dragOverEvent(e) {
+        EventHelpers.preventDefault(e);
+    }
+
+
+    function dropEvent(e) {
+        this.innerHTML = e.dataTransfer.getData('Text');
+        EventHelpers.preventDefault(e);
+    }
+
+}
+
+
+//EventHelpers.addPageLoadEvent('dragObject.init');
