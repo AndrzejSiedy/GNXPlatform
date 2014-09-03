349b20b66c4d58cee8f06ba6950145c8e6ef5d0f
wilk666 <andre@cartomatic.pl>
2014-09-03 13:38:44 +0200

    Add jQuery plugin to show/hide loadMask

--- Portal/Portal/Scripts/app/Utils/Utils.js
+++ Portal/Portal/Scripts/app/Utils/Utils.js
@@ -79,3 +79,36 @@ $.getUuid = function() {
     });
     return uuid;
 };
+
+
+
+/**
+ * Plugin to show loadmask on given div, a, span elements
+ * usage: $('#west-pane-div').showLoadMask();
+ */
+$.fn.showLoadMask = function () {
+    // generate load mask id
+    var lmId = $.getUuid();
+    // assign loadmask id to DOM object data
+    this.data( "_loadMaskId_", lmId );
+
+    var html = '<div id="' + lmId + '" style="position: absolute;top: 0px;left: 0px;height: 100%;width: 100%;background-color: transparent;display: none;">' +
+                    '<img src="/Content/Images/loading-blue.gif" alt="Loading" style="position: absolute;left: 50%;top: 50%;margin-left: -100px;margin-top: -50px;height: auto;width: 100px;" />' +
+                '</div>';
+
+    this.append(html);
+    $("#" + lmId).show();
+}
+
+/**
+ * Plugin to hide loadmask assigned to given div, a, span elements if shown using "showLoadMask" plugin
+ * usage: $('#west-pane-div').hideLoadMask();
+ */
+$.fn.hideLoadMask = function () {
+    // get assigned loadmask id from DOM object data
+    var lmId = this.data("_loadMaskId_");
+    $("#" + lmId).hide();
+    this.removeData("_loadMaskId_");
+    this.remove("#" + lmId);
+    
+}
