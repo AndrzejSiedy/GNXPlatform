21f4cda92ba6d77f345563d7a376b1467ba57782
wilk666 <andre@cartomatic.pl>
2014-09-03 10:33:30 +0200

    Untested solution to re-logging error
    "The provided anti-forgery token was meant for a different claims-based
    user than the current user."

--- Portal/Portal/Global.asax.cs
+++ Portal/Portal/Global.asax.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
 using System.Web;
+using System.Web.Helpers;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
@@ -14,6 +15,22 @@ namespace Portal
     {
         protected void Application_Start()
         {
+            // this meant to proterct logging error after user re-logs after while
+            // general exception was:
+            // "The provided anti-forgery token was meant for a different claims-based user than the current user."
+            //-------------------------
+            AntiForgeryConfig.SuppressIdentityHeuristicChecks = true;
+            Exception ex = Server.GetLastError();
+            if (ex is HttpAntiForgeryException)
+            {
+                Response.Clear();
+                Server.ClearError(); //make sure you log the exception first
+                Response.Redirect("~/Home/Index", true);
+            }
+            //-------------------------
+
+
+
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
