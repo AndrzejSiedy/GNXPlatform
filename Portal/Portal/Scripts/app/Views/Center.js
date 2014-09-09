d7abf4a58a04e037f50656a1e20d685710c3db44
wilk666 <andre@cartomatic.pl>
2014-09-09 16:15:57 +0200

    Add Center panel view with iframed Apache Shindig ref

---
+++ Portal/Portal/Scripts/app/Views/Center.js
@@ -0,0 +1,40 @@
+﻿/**
+ *  Init Center container UI
+ * Will hold Shinding iframed container   
+*/
+Gnx.Center = function () {
+
+    var _initialized = false;
+    this.initialized = false;
+
+    var _init = function () {
+        var html = '<iframe src="' + getShindigIframeUrl() + '" width="100%" height="100%" frameBorder="0"></iframe>'
+        $('#center-inner').html(html);
+    }
+
+    var getShindigIframeUrl = function () {
+        return shindigContainerUrl + getSignalRParams();
+    }
+
+    var getSignalRParams = function(){
+        // pass signalR setup information
+        //var signalRHubsUrl = '@TempData["signalRHubsUrl"]';
+        //var hubName = '@TempData["hubName"]';
+        //var roomId = '@TempData["roomId"]';
+
+        return '?signalRHubsUrl=' + signalRHubsUrl + '&hubName=' + hubName + '&roomId=' + roomId;
+    }
+
+    this.init = function () {
+
+        if (_initialized) return;
+
+        _init();
+
+        this.initialized = true;
+
+        _initialized = true;
+
+        return this.initialized;
+    }
+};
